c3f0323f2d99283c236a5446597d42e420442860
Matej Muller <matejmuller@Matejs-MacBook-Air.local>
2023-10-17 22:38:51 +0200

    Riješen JS-405

---
+++ Programsko Inženjerstvo/Polja i objekti/JS-405.js
@@ -0,0 +1,140 @@
+let fakultet = {
+    naziv: 'Fakultet Informatike u Puli',
+    adresa: 'Fipu 123',
+    studenti: {
+        preddiplomski: {
+            prvaGodina: {
+                prviUpis: [
+                    {
+                        ime: 'Marko',
+                        prezime: 'Marković',
+                        JMBAG: '12345',
+                    },
+                    {
+                        ime: 'Iva',
+                        prezime: 'Ivić',
+                        JMBAG: '67890',
+                    },
+                ],
+                drugiUpis: [
+                    {
+                        ime: 'Lucija',
+                        prezime: 'Lucić',
+                        JMBAG: '54321',
+                    },
+                ],
+            },
+            drugaGodina: [
+                {
+                    ime: 'Marko',
+                    prezime: 'Marković',
+                    JMBAG: '12345',
+                },
+                {
+                    ime: 'Iva',
+                    prezime: 'Ivić',
+                    JMBAG: '67890',
+                },
+                {
+                    ime: 'Lucija',
+                    prezime: 'Lucić',
+                    JMBAG: '54321',
+                },
+            ],
+            trećaGodina: [
+                {
+                    ime: 'Marko',
+                    prezime: 'Marković',
+                    JMBAG: '12345',
+                },
+                {
+                    ime: 'Iva',
+                    prezime: 'Ivić',
+                    JMBAG: '67890',
+                },
+                {
+                    ime: 'Lucija',
+                    prezime: 'Lucić',
+                    JMBAG: '54321',
+                },
+            ],
+        },
+        diplomski: [
+            {
+                ime: 'Marko',
+                prezime: 'Marković',
+                JMBAG: '12345',
+            },
+            {
+                ime: 'Iva',
+                prezime: 'Ivić',
+                JMBAG: '67890',
+            },
+            {
+                ime: 'Lucija',
+                prezime: 'Lucić',
+                JMBAG: '54321',
+            },
+        ],
+    },
+    osoblje: {
+        čistačice: [
+            {
+                ime: 'Marija',
+                prezime: 'Marić',
+            },
+            {
+                ime: 'Ivana',
+                prezime: 'Ivić',
+            },
+        ],
+        profesori: [
+            {
+                ime: 'Marko',
+                prezime: 'Marković',
+                kolegij: 'Web Aplikacije',
+                asistent: 'Marko Marković',
+            },
+            {
+                ime: 'Iva',
+                prezime: 'Ivić',
+                kolegij: 'Programsko Inženjerstvo',
+                asistent: 'Marko Marković',
+            },
+            {
+                ime: 'Lucija',
+                prezime: 'Lucić',
+                kolegij: 'Statistika',
+                asistent: 'Marko Marković',
+            },
+        ],
+        asistenti: [
+            {
+                ime: 'Marko',
+                prezime: 'Marković',
+            },
+            {
+                ime: 'Iva',
+                prezime: 'Ivić',
+            },
+            {
+                ime: 'Lucija',
+                prezime: 'Lucić',
+            },
+        ],
+    },
+    ucionice: [
+        {
+            broj: '101',
+            kapacitet: 30,
+        },
+        {
+            broj: '102',
+            kapacitet: 25,
+        },
+        {
+            broj: '103',
+            kapacitet: 40,
+        },
+    ],
+};
