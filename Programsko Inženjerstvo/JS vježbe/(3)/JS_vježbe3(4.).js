643c4b4880801ecfd6d548961c76804f56a8a210
Matej Muller <matejmuller@Matejs-MacBook-Air.local>
2023-10-22 12:59:56 +0200

    Nedovršen JS_vježbe3(4.)

---
+++ Programsko Inženjerstvo/JS vježbe/(3)/JS_vježbe3(4.).js
@@ -0,0 +1,13 @@
+const f = (n) => {
+    let arr = [];
+    let brojac = 1;
+
+    for (let i = 1; i !== n; i++) {
+        arr.push(i);
+    }
+
+    console.log(arr);
+};
+
+INPUT = 12;
+OUTPUT = f(INPUT); //[3,4,5]
